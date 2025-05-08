e64e763c84be1fe3de517fb1f0ccca713a9943f6
Jacob H <jacobhellebrand@csu.fullerton.edu>
2025-05-08 14:03:49 -0700

    added fake data to all the pages

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import { ChatBubbleBottomCenterTextIcon,ChartBarIcon, Cog6ToothIcon,CreditCardIcon,ArrowTrendingUpIcon  } from '@heroicons/react/24/solid';
 export function Sidebar({ isOpen }) {
     return (
       <aside
@@ -12,11 +12,37 @@ export function Sidebar({ isOpen }) {
 
         <nav>
           <ul className="flex flex-col gap-4">
-            <li><Link to="/aiassiant" className="hover:text-white">AI Assistant</Link></li>
-            <li><Link to="/goals" className="hover:text-white">Goals</Link></li>
-            <li><Link to="/budgets" className="hover:text-white">Budget Plans</Link></li>
-            <li><Link to="/trends" className="hover:text-white">Trends</Link></li>
-            <li><Link to="/settings" className="hover:text-white">Settings</Link></li>
+          <li>
+            <Link to="/aiassiant" className="hover:text-white flex items-center gap-2">
+              <ChatBubbleBottomCenterTextIcon className="w-5 h-5" />
+              AI Assistant
+            </Link>
+         </li>
+
+         <li>
+              <Link to="/goals" className="hover:text-white flex items-center gap-2">
+              <ArrowTrendingUpIcon  className="w-5 h-5" />
+              Financial Goals
+              </Link>
+            </li>
+            <li>
+              <Link to="/budgets" className="hover:text-white flex items-center gap-2">
+              <CreditCardIcon className="w-5 h-5" />
+              Budget Plans
+              </Link>
+            </li>
+            <li>
+              <Link to="/trends" className="hover:text-white flex items-center gap-2">
+              <ChartBarIcon className="w-5 h-5" />
+              Trends
+              </Link>
+            </li>
+            <li>
+              <Link to="/settings" className="hover:text-white flex items-center gap-2">
+              <Cog6ToothIcon className="w-5 h-5" />
+              Settings
+              </Link>
+            </li>
           </ul>
         </nav>
       </aside>
